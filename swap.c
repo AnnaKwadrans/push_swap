caca38eb455f572de8505cd2a3f40d7e22a1f148
Anna Kwadrans <akwadran@c2r18s2.42madrid.com>
2025-01-09 00:51:08 +0100

    swap

--- swap.c
+++ swap.c
@@ -6,13 +6,22 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 23:16:32 by akwadran          #+#    #+#             */
-/*   Updated: 2025/01/08 23:18:25 by akwadran         ###   ########.fr       */
+/*   Updated: 2025/01/09 00:50:35 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_list stack)
+void	swap(t_list *stack)
 {
+	t_list	*aux;
 	
+	if (stack->next != NULL)
+	{
+		aux = stack;
+		aux->content = stack->content;
+		aux->next = stack->next;
+
+		
+	}
 }
